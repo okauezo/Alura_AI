c8cf646f71a3ae2d5388a556e150a393027a100e
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-06-14 21:22:34 -0300

    Adicionando a embedding open source miniLM-L6-v2

--- embeddings/embedins.py
+++ embeddings/embedins.py
@@ -1,1 +1,16 @@
 from sentence_transformers import SentenceTransformer
+
+modelo = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+texto = "Estou aprendendo embeddings na Alura"
+
+vetor = modelo.encode(texto)
+
+print("Texto original:")
+print(texto)
+
+print("\nPrimeiros 5 números do vetor:")
+print(vetor[:5])
+
+print("\nTamanho do vetor:")
+print(len(vetor))
