9a4765c7a8d0f6b74b772828dfdab156f6aa885f
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-06-14 22:28:32 -0300

    Salva textos, vetores e metadados na VectorStore

--- embeddings/vectorstore_chroma.py
+++ embeddings/vectorstore_chroma.py
@@ -16,3 +16,17 @@ client = chromadb.PersistentClient(path="./chroma_db")
 collection = client.get_or_create_collection(name="documentos_alura")
 
 ids = ["doc1", "doc2", "doc3"]
+
+metadados = [
+    {"tipo": "exames"},
+    {"tipo": "agendamento"},
+    {"tipo": "atendimento"}
+]
+
+collection.upsert(
+    ids=ids,
+    documents=textos,
+    embeddings=vetores.tolist(),
+    metadatas=metadados
+)   
+
