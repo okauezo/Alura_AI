9bb305f7363c431631962aabc5c2275b147ac410
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-06-23 17:30:48 -0300

    Passando os parametros para fazer a comparação das perguntas e transformar em vetores

--- embeddings/consulta.py
+++ embeddings/consulta.py
@@ -3,3 +3,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 modelo = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
+documentos = [
+
+    "Como criar uma AI para auxiliar no atendimento ao cliente?",
+    "Quais são as melhores práticas para implementar uma IA no atendimento ao cliente?",
+    "Como medir o impacto da IA no atendimento ao cliente?"
+]
+
+
+
