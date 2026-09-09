915f91db947e7ee76d894ebe1b625030b6aae1fa
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-09-08 21:59:12 -0300

    Importando o Langchain.Prompts

--- LangChain_AI/Modelo_Viagem.py
+++ LangChain_AI/Modelo_Viagem.py
@@ -1,4 +1,5 @@
 from langchain_anthropic import ChatAnthropic
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +9,8 @@ api_key = os.getenv("ANTHROPIC_API_KEY")
 numero_de_dias = 15
 numero_de_pessoas = 4
 atividade = "praia"
+
+
 
 prompt = f"Planeje uma viagem de {numero_de_dias} dias para {numero_de_pessoas} pessoas com foco em atividades de {atividade}. Forneça um itinerário detalhado, incluindo sugestões de hospedagem, restaurantes e atrações turísticas."
 modelo = ChatAnthropic(
